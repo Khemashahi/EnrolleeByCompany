db576375af617375b69493c17c723993ab1f871b
Khemashahi <129611854+Khemashahi@users.noreply.github.com>
2023-04-03 21:02:03 +0530

    Update EnrolleeProcessor.java
    Updated

--- EnrolleesByCompany/src/com/poc/enrollees/EnrolleeProcessor.java
+++ EnrolleesByCompany/src/com/poc/enrollees/EnrolleeProcessor.java
@@ -1,28 +1,31 @@
 package com.poc.enrollees;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 public class EnrolleeProcessor {
 
-	public void seperateEnrollesByCompanyName(String inputFile, String outputDir) {
-       
+	public void seperateEnrollesByCompanyName(String inputFile, String outputDir) throws FileNotFoundException {
         Map<String, List<Enrollee>> enrolleesByInsurance = new TreeMap<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(inputFile))) {
-        	 String header = br.readLine();
+        try (Scanner scanner = new Scanner(new File(inputFile))) {
+			String header = scanner.nextLine();
         	String line;
-            while ((line = br.readLine()) != null) {
-                String[] coloumns = line.split(",");
-                String userId = coloumns[0];
-                String firstAndLastName = coloumns[1];
-                Integer version = Integer.parseInt(coloumns[2]);
-                String insuranceCompany = coloumns[3];
-
+        	while (scanner.hasNextLine()) {
+				String[] columns = scanner.nextLine().split(",");
+				String userId = columns[0];
+				String firstAndLastName = columns[1];
+				Integer version = Integer.parseInt(columns[2]);
+				String insuranceCompany = columns[3];
                 Enrollee enrollee = new Enrollee(userId, firstAndLastName,  version, insuranceCompany);
 
                 if (!enrolleesByInsurance.containsKey(insuranceCompany)) {
